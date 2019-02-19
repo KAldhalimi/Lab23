831a9d99143e6e4ab95bcf76ba96014b8089dc08
KAldhalimi <karrarmajed@gmail.com>
2019-02-19 17:45:39 -0500

    initial

---
+++ Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lab23.Models
+{
+    
+    
+
+        public class Item
+        {
+        [Key]
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public int Stock { get; set; }
+        }
+
+        public class DBItemContext : DbContext
+        {
+            public DbSet<Item> Items { get; set; }
+        }
+    }
